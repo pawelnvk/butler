b77e491c5419619b6a2b73b58ef9ee116b2d6377
Paweł Nowak <pawel.nowak@netguru.co>
2018-06-23 17:26:56 +0200

    Add enzyme to project.

--- client/src/components/views/App/App.test.tsx
+++ client/src/components/views/App/App.test.tsx
@@ -1,11 +1,10 @@
+import { shallow } from 'enzyme';
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
+  const component = shallow(<App />);
 
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  expect(component.length).toBe(1);
 });
